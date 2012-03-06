65d79a5d15f30e02cf416819dfcf380941d93db6
Bala Paranj <bparanj@gmail.com>
2012-03-05 17:49:49 -0800

    Readme added. First test passes.

--- rock/lib/angry_rock/game.rb
+++ rock/lib/angry_rock/game.rb
@@ -1,4 +1,7 @@
 module AngryRock
   class Game
+    def winner
+      "Green_Day"
+    end
   end
 end
--- rock/spec/angry_rock/game_spec.rb
+++ rock/spec/angry_rock/game_spec.rb
@@ -2,7 +2,15 @@ require 'spec_helper'
 
 module AngryRock
   describe Game do
-    it "picks paper as the winner over rock"
+    it "picks paper as the winner over rock" do
+      player_one = stub(:player, :name => "Green_Day", :choose => "paper")
+      player_two = stub(:player, :name => "minder", :choose => "rock")
+      
+      game = Game.new(player_one, player_two)
+      game.winner.should == "Green_Day"
+      
+    end
+    
     it "picks scissors as the winner over paper"
     it "picks rock as the winner over scissors "
     
