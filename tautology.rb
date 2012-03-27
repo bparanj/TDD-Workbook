0ea1cc2c37c932e954df5131bc492a963697bf91
Bala Paranj <bparanj@gmail.com>
2012-03-26 21:34:39 -0700

    Update tautology example

--- tautology.rb
+++ tautology.rb
@@ -1,7 +1,7 @@
 describe "Don't mock yourself out" do
   it "should illustrate tautology" do
-    paul = stub(:paul, :current_weight => 585)
+    paul = stub(:paul, :age => 20)
     
-    paul.current_weight.should == 585
+    paul.age.should == 20
   end
 end
